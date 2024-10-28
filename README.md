4c92c7d908e0a1d52bc7aab2bccd1125fb223a0b
ashen-hn <hosseinnavaiy3@gamil.com>
2024-10-28 22:17:16 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# FirstPrac" 
+"# hn" 
---
+++ index.js
@@ -0,0 +1,40 @@
+//تمرین 1//
+
+
+// a ={name:'ali', age:25,};
+// console.log(a.age);
+
+// let d =['amir','hosein','sanaz',];
+// console.log(d[2]);
+
+fruit =['apple', 'banana','orange','grape','mango'];
+input ='apple';
+test = 0;
+for(let x = 0; x < fruit.length; x++) {
+    if(fruit[x]== input ){
+        test = 1;
+    }
+}
+if(test == 1){
+    console.log('darim');
+}
+else{
+    console.log('nadarim');
+}
+
+// anbar =['y','y','r','r'];
+// tedadZard = 0;
+// tedadGhermez = 0;
+// for(let i=0;  i<anbar.length;  i++){
+//     if(anbar[i]=='y'){
+//         tedadZard = tedadZard + 1;
+//     }
+//     else if(anbar[i]=='r'){
+//         tedadGhermez++ ;
+//     }
+// }
+// console.log("tedad zard:");
+// console.log(tedadZard);
+// console.log("tedad ghermez:");
+// console.log(tedadGhermez);
+
